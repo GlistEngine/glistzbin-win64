683a8593f724b55851a83ee55c7dce6210e77ba4
Noyan Culum <noyan.culum@gamelab.istanbul>
2023-03-13 21:29:17 +0300

    clang64 files uploaded

---
+++ clang64/include/llvm/Support/VCSRevision.h
@@ -0,0 +1,2 @@
+#define LLVM_REVISION "de4afab29f30e3f6b1f5887fe0cef32c5f09963e"
+#define LLVM_REPOSITORY "https://github.com/msys2/MINGW-packages"
---
+++ clang64/lib/cmake/glm/glmConfig.cmake
@@ -0,0 +1,23 @@
+set(GLM_VERSION "0.9.9")
+
+
+####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
+####### Any changes to this file will be overwritten by the next CMake run ####
+####### The input file was glmConfig.cmake.in                            ########
+
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
+
+macro(set_and_check _var _file)
+  set(${_var} "${_file}")
+  if(NOT EXISTS "${_file}")
+    message(FATAL_ERROR "File or directory ${_file} referenced by variable ${_var} does not exist !")
+  endif()
+endmacro()
+
+####################################################################################
+
+set_and_check(GLM_INCLUDE_DIRS "${PACKAGE_PREFIX_DIR}/include")
+
+if (NOT CMAKE_VERSION VERSION_LESS "3.0")
+    include("${CMAKE_CURRENT_LIST_DIR}/glmTargets.cmake")
+endif()
